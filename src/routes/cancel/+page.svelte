e9fd3949745c24399ad9fb2353908a3de6b3b84a
secretarybird97 <81328194+secretarybird97@users.noreply.github.com>
2023-05-27 21:01:00 -0700

    dawd

--- src/routes/cancel/+page.svelte
+++ src/routes/cancel/+page.svelte
@@ -1,3 +1,9 @@
+<script lang="ts">
+	import { setModeCurrent } from '@skeletonlabs/skeleton';
+
+	setModeCurrent(true);
+</script>
+
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="grid grid-cols-3 gap-4">
 		<div class="col-span-3">
