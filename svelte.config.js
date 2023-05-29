91b98a7ad04474560eabc916ce992d1e46cbf5d9
secretarybird97 <81328194+secretarybird97@users.noreply.github.com>
2023-05-28 18:09:03 -0700

    vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,6 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const dev = process.argv.includes('dev');
 /** @type {import('@sveltejs/kit').Config} */
 
 const config = {
@@ -10,10 +9,9 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter(),
-		paths: {
-			base: dev ? '' : process.env.BASE_PATH
-		}
+		adapter: adapter({
+			runtime: 'nodejs18.x'
+		})
 	}
 };
 
